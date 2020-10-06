e830880e1cdf34c6df4b8729de5ba3a7ea7da0ab
Braxton Harley <braxton.2012.harley@gmail.com>
2020-10-06 12:34:42 -0500

    added footer, sidebar, and css files

--- src/Video.js
+++ src/Video.js
@@ -1,10 +1,20 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import VideoFooter from "./VideoFooter";
+import VideoSidebar from "./VideoSidebar";
 import "./Video.css";
 
-function Video() {
+function Video({ url, channel, description, song, likes, messages, shares }) {
+  const [playing, setPlaying] = useState(false);
   const videoRef = useRef(null);
+
   const onVideoPress = () => {
-    videoRef.current.play();
+    if (playing) {
+      videoRef.current.pause();
+      setPlaying(false);
+    } else {
+      videoRef.current.play();
+      setPlaying(true);
+    }
   };
 
   return (
@@ -12,10 +22,12 @@ function Video() {
       <video
         className="video__player"
         loop
-        ref={videoRef}
         onClick={onVideoPress}
-        src="https://www.tiktok.com/@lone.wolfy/video/6873118144106646785?lang=en"
+        ref={videoRef}
+        src={url}
       ></video>
+      <VideoFooter channel={channel} description={description} song={song} />
+      <VideoSidebar likes={likes} messages={messages} shares={shares} />
     </div>
   );
 }
